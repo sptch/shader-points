7e3fc0137d62583b29fcaef4cebc7a5e38d1ce29
Herman Mitish <german.mitish@gmail.com>
2020-11-28 10:41:11 +0200

    Removed big point file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React, { useMemo, useRef } from "react"
 import ReactDOM from "react-dom"
 import { Canvas, extend, useFrame, useThree } from "react-three-fiber"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import vert from "./vert.glsl"
-import frag from "./frag.glsl"
 import "./styles.css"
+
+const frag = `
+uniform float time;
+void main() {
+   float z = 1.0 - (gl_FragCoord.z / gl_FragCoord.w) / 20.0;
+   gl_FragColor = vec4(sin(time * 2.0) * z, cos(time) * z, atan(time) * z, 1.0);
+}`
+
+const vert = `
+uniform float time;
+attribute vec3 velocity;
+attribute vec3 acceleration;
+void main() {
+    vec3 pos = position;
+    gl_Position = projectionMatrix 
+        * modelViewMatrix
+        * vec4(
+            vec3(
+                sin(time * 4.0) + pos[0] + cos(time / velocity[0]) * 1.0 * velocity[0],
+                cos(time + time * 4.0) + pos[1] * 2.0 * sin(time / velocity[1]) * velocity[1],
+                sin(time + time + time * 4.0) + pos[2] * 2.0 * cos(time / velocity[2]) * 1.0 * velocity[2]), 1.0);
+    gl_PointSize = 5.0;
+}`
 
 extend({ OrbitControls })
 
@@ -44,7 +65,13 @@ function Particles({ pointCount }) {
         <bufferAttribute attachObject={["attributes", "velocity"]} count={velocities.length / 3} array={velocities} itemSize={3} />
         <bufferAttribute attachObject={["attributes", "acceleration"]} count={accelerations.length / 3} array={accelerations} itemSize={3} />
       </bufferGeometry>
-      <shaderMaterial attach="material" uniforms={uniforms} vertexShader={vert} fragmentShader={frag} vertexColors />
+      <shaderMaterial attach="material"   
+        args={[{
+          uniforms: uniforms,
+          vertexShader: vert,
+          fragmentShader: frag,
+        }]}
+      />
     </points>
   )
 }
