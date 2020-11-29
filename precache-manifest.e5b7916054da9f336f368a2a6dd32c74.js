ae9a2d4ed5f114f806e1afd1b957270eca73f26e
germanmitish <germanmitish@users.noreply.github.com>
2020-11-29 22:31:46 +0000

    deploy: 80b9c586e9cea4c1557cbe6bf5a3883714b97e52

---
+++ precache-manifest.e5b7916054da9f336f368a2a6dd32c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5923cc371a7b06a47b32fd216582684",
+    "url": "/shader-points/index.html"
+  },
+  {
+    "revision": "c9b4cce0ca5151403232",
+    "url": "/shader-points/static/css/main.84523e09.chunk.css"
+  },
+  {
+    "revision": "38488bdade9438ea872f",
+    "url": "/shader-points/static/js/2.1828f943.chunk.js"
+  },
+  {
+    "revision": "c9b4cce0ca5151403232",
+    "url": "/shader-points/static/js/main.ec82bfa9.chunk.js"
+  },
+  {
+    "revision": "aa25c80b5aa6abc5a8e4",
+    "url": "/shader-points/static/js/runtime-main.b38231c7.js"
+  },
+  {
+    "revision": "1c4f20fe5d1b03d5542c81e3008221e0",
+    "url": "/shader-points/static/media/32FFF.1c4f20fe.xyz"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35ba9e44b98ec619e2c347edd7b76a7e.js"
+  "/shader-points/precache-manifest.e5b7916054da9f336f368a2a6dd32c74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shader-points/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
